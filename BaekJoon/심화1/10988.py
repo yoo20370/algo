c1f5d5ac3f8688494862a33939a91faebc6df1f9
yoo20370 <yoo20370@gmail.com>
2024-10-22 16:03:03 +0900

    20241022 160200 algo

--- BaekJoon/심화1/10988.py
+++ BaekJoon/심화1/10988.py
@@ -1,1 +1,11 @@
-string = input()
+data = input()
+
+def checkFunc(string) :
+
+    length = len(string)
+    for i in range(length // 2) :
+        if string[i] != string[length - 1 - i] :
+            return 0
+    return 1
+
+print(checkFunc(data))
--- BaekJoon/심화1/2444.py
+++ BaekJoon/심화1/2444.py
@@ -1,9 +1,25 @@
 N = int(input())
 
-for i in range(N) :
+for i in range(1, N+1) :
     print(" " * (N -i), end="")
-    print("*" * i, end="")
-    print(" "* (N -i), end="")
+    print("*" * (2*i-1))
+    
 
+for i in range(N-1, 0, -1) :
+    print(" " * (N -i), end="")
+    print("*" * (2*i-1))
+    
 
+# 별은 증가하고 공백은 감소 
+# 별 1개 공백 0개
+
+# 별 3개 공백 2개 
+
+# 별 5개 공백 4개 
+
+# 별 9개 공백 8개 
+
+# 별 11개 공백 10개
+
+# 별의 개수는 2N-1
 
