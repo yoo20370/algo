31c0cfff3dce82939d8d3a14ed20fe1fac979c37
yoo20370 <yoo20370@gmail.com>
2024-11-11 22:15:24 +0900

    20241111 221600 algo

--- BaekJoon/완전탐색/2231.py
+++ BaekJoon/완전탐색/2231.py
@@ -1,19 +1,39 @@
+# N = int(input())
+
+# def func(n) :
+#     if n == 1 :
+#         return 0
+#     min = 1000001
+
+#     for i in range(1, n) :
+#         temp = 0
+#         for j in str(i) :
+#             temp += int(j)
+#         # N 구하기 
+#         if i + temp == n :
+#             # N의 생성자 중 최소값보다 작은 값인지 체크 
+#             if min > i + temp :
+#                 min = i
+    
+#     if min == 1000001 :
+#         return 0
+#     else :
+#         return min
+    
+# print(func(N))
+
 N = int(input())
 
 def func(n) :
     if n == 1 :
         return 0
     min = 1000001
-    
-    for i in range(1, n) :
-        temp = 0
-        for j in str(i) :
-            temp += int(j)
-        # N 구하기 
-        if i + temp == n :
-            # N의 생성자 중 최소값보다 작은 값인지 체크 
-            if min > i + temp :
-                min = i
+
+    for i in range(max(1, n - (len(str(n)) * 9)), n) :
+        sumVal = sum(map(int, list(str(i))))
+        if i + sumVal == n :
+            if min > i + sumVal :
+                min = i        
     
     if min == 1000001 :
         return 0
@@ -23,17 +43,41 @@ def func(n) :
 print(func(N))
 
 
+# # 다른 사람 1
+# n = int(input())
 
-n = int(input())
+# for i in range(n):
+#     s = i
+#     r = i
+#     while i > 0:
+#         r += i % 10
+#         i = i // 10
+#     if r == n:
+#         print(s)
+#         break
+# else:
+#     print(0)
 
-for i in range(n):
-    s = i
-    r = i
-    while i > 0:
-        r += i % 10
-        i = i // 10
-    if r == n:
-        print(s)
-        break
-else:
-    print(0)
+# # 다른 사람 2
+# n = int(input())
+# result = None
+# placesSum = sum(map(int, list(str(n))))
+# for i in range(max(1, n - (len(str(n)) * 9)), n):
+#     if sum(map(int, list(str(i)))) + i == n:
+#         result = i
+#         break
+# if result is None:
+#     print(0)
+# else:
+#     print(result)
+
+# # 다른 사람 3
+# N = int(input())
+# s = max(N - 63, 1)
+# for n in range(s,N):
+#     t = n + sum(map(int, str(n)))
+#     if t == N:
+#         print(n)
+#         break
+# else:
+#     print(0)
