93a607e9728042c68aa5edabb48f8060941bc617
yoo20370 <yoo20370@gmail.com>
2024-11-13 01:02:30 +0900

    20241113 010220 algo

--- BaekJoon/정렬/2751.py
+++ BaekJoon/정렬/2751.py
@@ -1,5 +1,48 @@
-# 병합 정렬 
-# 재귀 함수 
+# # 병합 정렬 
+# # 재귀 함수 
+
+# N = int(input())
+
+# data = list()
+# for i in range(N) :
+#     data.append(int(input()))
+
+# def mergeSort(arr) :
+#     length = len(arr) 
+
+#     if length == 1 :
+#         return arr 
+
+#     pl = 0
+#     pr = length - 1
+#     mid = length // 2
+
+#     left = mergeSort(arr[:mid])
+#     right = mergeSort(arr[mid:])
+
+#     result = list()
+#     left_idx = 0
+#     right_idx = 0
+
+#     while left_idx < len(left) and right_idx < len(right) :
+#         if left[left_idx] < right[right_idx] :
+#             result.append(left[left_idx])
+#             left_idx += 1
+#         else :
+#             result.append(right[right_idx])
+#             right_idx += 1
+    
+#     result.extend(left[left_idx:])
+#     result.extend(right[right_idx:])
+
+#     return result
+
+# data = mergeSort(data)
+
+# for i in data :
+#     print(i)
+
+# 퀵 정렬 
 
 N = int(input())
 
@@ -7,37 +50,32 @@ data = list()
 for i in range(N) :
     data.append(int(input()))
 
-def mergeSort(arr) :
-    length = len(arr) 
+def quickSort(arr, left, right) : 
 
-    if length == 1 :
-        return arr 
-
-    pl = 0
-    pr = length - 1
-    mid = length // 2
-
-    left = mergeSort(arr[:mid])
-    right = mergeSort(arr[mid:])
-
-    result = list()
-    left_idx = 0
-    right_idx = 0
-
-    while left_idx < len(left) and right_idx < len(right) :
-        if left[left_idx] < right[right_idx] :
-            result.append(left[left_idx])
-            left_idx += 1
-        else :
-            result.append(right[right_idx])
-            right_idx += 1
     
-    result.extend(left[left_idx:])
-    result.extend(right[right_idx:])
+    pl = left
+    pr = right
+    # (left + right) // 2 처럼 인덱스를 계산하면 arr[p] 값이 중간에 변경될 수 있음 
+    # arr[p]의 경우 pl이 p를 선택하고 pr이 다른 원소를 선택하다가 변경될 수 있음 (항상 그런 것은 아님)
+    p = arr[(left + right) // 2]
 
-    return result
 
-data = mergeSort(data)
+    while pl <= pr :
+        while arr[pl] < arr[p] :
+            pl += 1
+        while arr[pr] > arr[p] :
+            pr -= 1
+        
+        if pl <= pr :
+            arr[pl], arr[pr] = arr[pr], arr[pl]
+            pl += 1
+            pr -= 1
+    if left < pr :
+        quickSort(arr, left, pr )
+    if right > pl :
+        quickSort(arr, pl , right)
+
+quickSort(data,0, len(data) - 1)
 
 for i in data :
     print(i)
