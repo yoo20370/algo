ba5d3afe7b5ae02297a124e3470bf453a780f4f2
yoo20370 <yoo20370@gmail.com>
2024-11-13 09:55:03 +0900

    20241113 095500 algo

--- BaekJoon/정렬/2751.py
+++ BaekJoon/정렬/2751.py
@@ -59,7 +59,7 @@ def quickSort(arr, left, right) :
     # arr[p]의 경우 pl이 p를 선택하고 pr이 다른 원소를 선택하다가 변경될 수 있음 (항상 그런 것은 아님)
     p = arr[(left + right) // 2]
 
-
+    # 등호를 붙이는 이유는, 같은 위치에서의 교환을 통해 pl += 1, pr -=1를 수행하지 않아, 분할이 제대로 되지 않음 
     while pl <= pr :
         while arr[pl] < arr[p] :
             pl += 1
@@ -70,8 +70,12 @@ def quickSort(arr, left, right) :
             arr[pl], arr[pr] = arr[pr], arr[pl]
             pl += 1
             pr -= 1
+    
+    # 배열 원소가 하나가 아닐 떄를 고려한 조건문
     if left < pr :
         quickSort(arr, left, pr )
+        
+    # 배열 원소가 하나가 아닐 떄를 고려한 조건문
     if right > pl :
         quickSort(arr, pl , right)
 
