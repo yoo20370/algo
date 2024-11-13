5936032d397ff386d035543174b823ce3d21fff0
yoo20370 <yoo20370@gmail.com>
2024-11-13 21:20:42 +0900

    20241113 212050 algo

---
+++ BaekJoon/정렬/10814.py
@@ -0,0 +1,41 @@
+def quick(arr, left, right, option) :
+    pl = left
+    pr = right
+    p = arr[(pl + pr) // 2][option]
+
+    while pl <= pr :
+        while arr[pl][option] < p :
+            pl += 1
+        while p <  arr[pr][option] :
+            pr -= 1
+        if pl <= pr :
+            arr[pl], arr[pr] = arr[pr], arr[pl]
+            pl += 1
+            pr -= 1
+
+    if left < pr :
+        quick(arr, left, pr, option)
+    if pl < right :
+        quick(arr, pl, right, option)
+
+N = int(input())
+
+arr = list()
+for i in range(N) :
+    x, y = input().split()
+    arr.append([int(x), y, i])
+print(arr)
+
+quick(arr, 0, len(arr)-1, 0)
+
+s = 0
+for i in range(1, N + 1) :
+    if i == N or arr[s][0] != arr[i][0] :
+        quick(arr, s, i - 1, 2)
+        s = i
+
+for x, y, z in arr :
+    print(x, y)
+
+
+
