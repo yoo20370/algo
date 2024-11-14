7512f937c8c591418a7ec44a033da4bed5635c1e
yoo20370 <yoo20370@gmail.com>
2024-11-14 17:47:48 +0900

    20241114 174750 algo

--- BaekJoon/집합과 맵/1620.py
+++ BaekJoon/집합과 맵/1620.py
@@ -2,14 +2,40 @@ import sys
 
 N, M = map(int, input().split())
 
-listA = [[] for i in range(1000)]
+listA = [[] for _ in range(1000)]
+listB = [[] for _ in range(1000)]
 
-for i in range(N) :
+for i in range(1, N+1) :
     data = sys.stdin.readline().rstrip()
-    hashVal = i // 1000
+
+    hashVal = int(i) % 1000
+    hashVal2 = hash(data) % 1000
+
     listA[hashVal].append([i, data])
+    listB[hashVal2].append([i, data])
 
-print(listA)
 
-# for i in range(M) :
+result = list()
+def func(hashTable, data, n) :
+    # 숫자인 경우
+    if n == 0 :
+        hashVal = int(data) % 1000
+        for i in hashTable[hashVal] :
+            if int(i[0]) == int(data) :
+                result.append(i[1])
+    else :
+        hashVal = hash(data) % 1000
+        for i in hashTable[hashVal] :
+            if i[1] == data :
+                result.append(i[0])
+
+for i in range(M) :
+    data = sys.stdin.readline().rstrip()
+    if data.isdigit() :
+        func(listA, data, 0)
+    else :
+        func(listB, data, 1)
+
+for i in result :
+    print(i)
 
