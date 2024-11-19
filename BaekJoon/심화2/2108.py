9103c8fcc7a9433aad5eb6bdda796babc3402581
yoo20370 <yoo20370@gmail.com>
2024-11-19 14:18:38 +0900

    20241119 141900

--- BaekJoon/심화2/2108.py
+++ BaekJoon/심화2/2108.py
@@ -1,7 +1,53 @@
-import sys
+import sys, math
 
-N = int(sys.stdin.readline().split())
+N = int(sys.stdin.readline())
 
+dic = dict()
 listA = list()
 for i in range(N) :
-    listA.append(int(sys.stdin.readline().rstrip()))
+    data = int(sys.stdin.readline().rstrip())
+    listA.append(data)
+    
+    if dic.get(data) :
+        dic[data] = dic.get(data) + 1
+    else :
+        dic[data] = 1
+
+maxVal = max(dic.values())
+
+# 평균값
+avg = int(round(sum(listA) / N, 0))
+
+# 중앙값 
+if len(listA) == 1 :
+    mid = listA[0]
+else :
+    listA.sort()
+    mid = listA[len(listA) // 2]
+
+# 최빈값 
+keys = [k for k, v in dic.items() if v == maxVal]
+if len(keys) == 1: 
+    minCnt = keys[0]
+else :
+    keys.sort()
+    minCnt = keys[1]
+
+maxVal = max(dic.keys())
+minVal = min(dic.keys())
+
+# 범위값 
+degree = abs(maxVal - minVal)
+
+print(avg)
+print(mid)
+print(minCnt)
+print(degree)
+
+
+
+
+
+
+
+
