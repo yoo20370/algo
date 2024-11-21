e345f85d81727794c4c9f9175b5ffe979e55dd36
yoo20370 <yoo20370@gmail.com>
2024-11-21 22:55:09 +0900

    20241121 225510

--- BaekJoon/재귀/24060.py
+++ BaekJoon/재귀/24060.py
@@ -1,71 +1,13 @@
-# import sys
-
-# def merge_sort(arr, pl, pr, tmp) :
-#     if pl < pr :
-#         p = (pl + pr) // 2
-#         merge_sort(arr, pl, p, tmp) 
-#         merge_sort(arr, p + 1, pr, tmp)
-#         merge(arr, pl, p, pr, tmp)
-
-    
-
-# def merge(arr, pl, p, pr, temp) :
-#     i = pl 
-#     j = p + 1
-
-#     tmp = list()
-#     while i <= p and j <= pr :
-#         if arr[i] <= arr[j] :
-            
-#             tmp.append(arr[i])
-#             i += 1
-#         else :
-            
-#             tmp.append(arr[j])
-#             j += 1
-    
-#     while i <= p :
-        
-#         tmp.append(arr[i])
-#         i += 1
-    
-#     while j <= pr :
-        
-#         tmp.append(arr[j])
-#         j += 1
-#     i = pl
-#     t = 0
-    
-#     while i <= pr :
-#         temp.append(tmp[t])
-#         arr[i] = tmp[t]
-#         t += 1
-#         i += 1
-
-# N, M = map(int, sys.stdin.readline().split())
-# arr = list(map(int, sys.stdin.readline().split()))
-
-# tmp = list()
-# merge_sort(arr, 0, len(arr) - 1, tmp)
-
-# if len(tmp) >= M :
-#     print(tmp[M-1])
-# else :
-#     print(-1)
-
 import sys
 
-def merge_sort(arr, pl, pr, cnt) :
+def merge_sort(arr, pl, pr, tmp) :
     if pl < pr :
         p = (pl + pr) // 2
-        merge_sort(arr, pl, p, cnt) 
-        merge_sort(arr, p + 1, pr, cnt)
-        if 0 == merge(arr, pl, p, pr, cnt) :
-            return 0
+        merge_sort(arr, pl, p, tmp) 
+        merge_sort(arr, p + 1, pr, tmp)
+        merge(arr, pl, p, pr, tmp)
 
-    
-
-def merge(arr, pl, p, pr, cnt) :
+def merge(arr, pl, p, pr, temp) :
     i = pl 
     j = p + 1
 
@@ -93,10 +35,7 @@ def merge(arr, pl, p, pr, cnt) :
     t = 0
     
     while i <= pr :
-        global M 
-        if cnt == M :
-            print(tmp[t])
-            return 0
+        temp.append(tmp[t])
         arr[i] = tmp[t]
         t += 1
         i += 1
@@ -104,6 +43,12 @@ def merge(arr, pl, p, pr, cnt) :
 N, M = map(int, sys.stdin.readline().split())
 arr = list(map(int, sys.stdin.readline().split()))
 
-if 0 != merge_sort(arr, 0, len(arr) - 1, 1) :
+tmp = list()
+merge_sort(arr, 0, len(arr) - 1, tmp)
+
+if len(tmp) >= M :
+    print(tmp[M-1])
+else :
     print(-1)
+
 
--- BaekJoon/재귀/2447.py
+++ BaekJoon/재귀/2447.py
@@ -1,14 +1,73 @@
-import sys
+import sys 
 
-def hanoi(n, start, sub, end) :
+def star10(data, idx) :
+
+    N = len(data)
+    # 종료조건 
+    if N == 1 :
+        return data
     
-    if n > 1 :
-        hanoi(n-1, start, end ,sub) 
-    print(start, end)
-    if n > 1 :
-        hanoi(n-1, sub, start, end)  
+    l = N // 3
+    r = l * 2
+    
+    mid = '*' * l
+    
+    # 특정 범위에 해당된다면 중앙을 
+    if idx >= l and idx < r :
+        mid = ' ' * l
+
+    idx %= l
+
+    # 문제 축소 
+    left = star10(data[:l], idx)
+    if mid != ' ' * l  :
+        mid = star10(data[l:r], idx) 
+    right = star10(data[r:], idx)
+    
+    return left + mid + right
 
 N = int(sys.stdin.readline().rstrip())
 
-print(2**N - 1)
-hanoi(N,1,2,3)
+for i in range(N) :
+    print(star10("*"* N, i))
+
+# 다른 사람 풀이 
+# import sys
+# input = sys.stdin.readline
+
+# def draw_star(n):
+#     """ 
+#     크기 n의 별 패턴을 생성하는 재귀 함수
+#     """
+#     if n == 1:  
+#         # 크기가 1인 패턴은 별 하나
+#         return ["*"]
+    
+#     # N/3 크기의 패턴을 생성 (재귀적으로 호출)
+#     sub_pattern = draw_star(n // 3)
+#     pattern = []
+
+#     # 첫 번째와 세 번째 줄: 작은 패턴 3개를 이어 붙임
+#     for line in sub_pattern:
+#         pattern.append(line * 3)
+
+#     # 두 번째 줄: 가운데에 공백을 넣어 패턴 생성
+#     for line in sub_pattern:
+#         pattern.append(line + " " * (n // 3) + line)
+
+#     # 첫 번째와 세 번째 줄과 동일
+#     for line in sub_pattern:
+#         pattern.append(line * 3)
+
+#     return pattern
+
+# # 입력 받기
+# N = int(input())
+
+# # 크기 N의 별 패턴 생성
+# result = draw_star(N)
+
+# # 결과 출력
+# print("\n".join(result))
+
+# # sys.stdout.write("\n".join(result) + "\n") 출력 성능이 더 중요한 경우(chat gpt)
