dbe03efd98a5ae87fde5f422feee60f7a216b639
yoo20370 <yoo20370@gmail.com>
2024-11-23 10:15:17 +0900

    20241123 101520

---
+++ 바킹독/0x03/3273.py
@@ -0,0 +1,19 @@
+import sys 
+
+arr = [0] * 10
+
+max_val = 0
+max_idx = 0
+for i in sys.stdin.readline().rstrip() :
+    s = i
+    arr[int(i)] += 1
+    
+    # 6 혹은 9인 경우 
+    if i == '6' or i == '9' and arr[int(s)] >= max_val:
+        max_idx = int(s)
+        max_val = arr[int(i)]
+    elif arr[int(s)] > max_val :
+        max_idx = int(s)
+        max_val = arr[int(i)]
+
+print(max_idx, arr[max_idx])
