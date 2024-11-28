85105b622809b4e7aaa2aeb2b65a21834c9c0a88
yoo20370 <yoo20370@gmail.com>
2024-11-28 09:59:21 +0900

    20241128 095924

--- 1550.py
+++ 1550.py
@@ -1,5 +1,11 @@
+# import sys
+# N = sys.stdin.readline().rstrip()
+
+# print(int(N, 16))
+
 import sys
-N = sys.stdin.readline().rstrip()
 
-print(int(N, 16))
+N = int(sys.stdin.readline().rstrip())
 
+for i in range(N):
+    print(i+1)
