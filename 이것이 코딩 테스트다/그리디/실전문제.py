fd9be18b7de9fde951c531540bb88b3beb54a9c4
yoo20370 <yoo20370@gmail.com>
2024-11-29 15:13:31 +0900

    20241129 151330

--- 이것이 코딩 테스트다/그리디/실전문제.py
+++ 이것이 코딩 테스트다/그리디/실전문제.py
@@ -1,3 +1,39 @@
+## 최대 O(N) 시간 복잡도 
+
+# import sys
+
+# N, M, K = map(int, sys.stdin.readline().split())
+
+# arr = list(map(int, sys.stdin.readline().split()))
+
+# arr.sort(reverse=True)
+
+# def func(N, M, K) :
+
+#     cnt = 0 
+#     sum = 0
+#     if arr[0] == arr[1] :
+#         return M * arr[0]
+
+#     while M != 0 :
+#         if M >= K and cnt != K : 
+#             sum += arr[0] * K
+#             M -= K
+#             cnt = K
+#             continue
+#         elif cnt != K :
+#             sum += arr[0]
+#             cnt += 1
+#         else :
+#             sum += arr[1]
+#             cnt = 0 
+#         M -= 1
+    
+#     print(sum)
+
+# func(N,M,K)
+
+## 상수 시간 O(1) 시간 복잡도 
 import sys
 
 N, M, K = map(int, sys.stdin.readline().split())
@@ -6,22 +42,17 @@ arr = list(map(int, sys.stdin.readline().split()))
 
 arr.sort(reverse=True)
 
-def func(N, M, K) :
+def func(n, m, k) :
+    result = 0 
+    val = m // (k+1) 
+    remain = m % (k+1)
 
-    cnt = 0 
-    sum = 0
-    if arr[0] == arr[1] :
-        return M * arr[0]
+    print(val, remain)
+    result = (arr[0] * K + arr[1]) * val
 
-    while M != 0 :
-        if cnt != K :
-            sum += arr[0]
-            cnt += 1
-        else :
-            sum += arr[1]
-            cnt = 0 
-        M -= 1
-    
-    print(sum)
+    result += remain * arr[0]
+
+    print(result)
 
 func(N,M,K)
+
