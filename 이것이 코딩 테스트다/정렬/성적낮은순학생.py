e868d7d8d35350e08cd647363de2bd9564daa16e
yoo20370 <yoo20370@gmail.com>
2024-12-05 16:23:11 +0900

    20241205 162310

---
+++ 이것이 코딩 테스트다/정렬/성적낮은순학생.py
@@ -0,0 +1,36 @@
+# 정렬 라이브러리 풀이 
+# import sys
+
+# N = int(sys.stdin.readline().rstrip())
+
+# arr = list()
+
+# for i in range(N) :
+#     name, score = sys.stdin.readline().split()
+#     arr.append([name,int(score)])
+
+# def setting(data) :
+#     return data[1]
+
+# arr.sort(key = setting) 
+
+# for name, score in arr :
+#     print(name, end=" ")
+
+# 계수 정렬
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+
+MX = 101 
+# arr[0]은 수, arr[1]은 이름 저장 
+arr = [[0,list()] for i in range(MX)]
+for i in range(N) :
+    name, score = sys.stdin.readline().split()
+    arr[int(score)][0] += 1
+    arr[int(score)][1].append(name)
+
+
+for idx in range(MX) :
+    for cnt in range(len(arr[idx][1])) :
+        print(arr[idx][1][cnt], end=" ")
