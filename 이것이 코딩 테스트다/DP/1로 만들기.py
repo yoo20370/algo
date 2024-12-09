441b5ba96dce328257b24d640ec29356d7d8ece0
yoo20370 <yoo20370@gmail.com>
2024-12-09 21:56:53 +0900

    20241209 215655

--- 이것이 코딩 테스트다/DP/1로 만들기.py
+++ 이것이 코딩 테스트다/DP/1로 만들기.py
@@ -1,24 +1,50 @@
-import sys 
+# import sys 
+
+# sys.setrecursionlimit(100000)
+
+# N = int(sys.stdin.readline().rstrip())
+
+# MX = 10**6 + 1
+
+# dp = [0] * MX 
+
+# def makeOne(n) :
+#     if n == 1 :
+#         return 0
+    
+#     if dp[n] != 0 :
+#         return dp[n]
+
+#     result = makeOne(n-1) + 1
+#     if n % 3 == 0 :
+#         result = min(result, makeOne(n//3) + 1)
+#     if n % 2 == 0 :
+#         result = min(result, makeOne(n//2) + 1)
+    
+
+#     dp[n] = result
+#     return dp[n]
+
+
+# print(makeOne(N))
+
+import sys
 
 N = int(sys.stdin.readline().rstrip())
 
-def makeOne(n) :
-    if n == 1 :
-        return 0
-    cnt = 0 
-    while n != 1 :
-        cnt += 1
-        if n % 5 == 0 :
-            n = n // 5 
-        elif n % 3 == 0 :
-            n = n // 3
-        elif n % 2 == 0 :
-            n = n // 2
-        else :
-            n -= 1
-        
-        print(n)
-        
-    print(cnt)
+MX = 10**6 + 1
 
-print(makeOne(N))
+dp = [-1] * MX
+
+dp[1] = 0 
+for i in range(2,N+1) :
+
+    result = dp[i-1] + 1 
+    if i % 3 == 0 :
+        result = min(result, dp[i//3] + 1)
+    if i % 2 == 0 :
+        result = min(result, dp[i//2] + 1)
+    
+    dp[i] = result
+
+print(dp[N])
