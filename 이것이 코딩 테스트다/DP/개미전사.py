8328f2cd7e30269e81639997fb3df4641fb57c06
yoo20370 <yoo20370@gmail.com>
2024-12-09 23:40:47 +0900

    20241209 234050

--- 이것이 코딩 테스트다/DP/개미전사.py
+++ 이것이 코딩 테스트다/DP/개미전사.py
@@ -29,10 +29,10 @@ storage = list(map(int, sys.stdin.readline().split()))
 dp = [-1] * N
 
 dp[0] = storage[0]
-dp[1] = storage[1]
+dp[1] = max(storage[0], storage[1])
 
 maxVal = max(dp[0], dp[1])
 for i in range(2, N) :
-    maxVal = max(dp[i-1], dp[i-2] + storage[i])
+    dp[i] = max(dp[i-1], dp[i-2] + storage[i])
 
-print(maxVal)
+print(dp[N-1])
