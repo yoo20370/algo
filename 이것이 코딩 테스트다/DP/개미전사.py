d53bbd9a90154f1d8baba4b3041a2829bd5cf1c8
yoo20370 <yoo20370@gmail.com>
2024-12-09 23:07:19 +0900

    20241209 230730

---
+++ 이것이 코딩 테스트다/DP/개미전사.py
@@ -0,0 +1,38 @@
+# import sys
+
+# N = int(sys.stdin.readline().rstrip())
+
+# dp = [-1] * N 
+
+# storage = list(map(int, sys.stdin.readline().split()))
+
+# def findMax(arr, curr) :
+#     if len(arr) <= curr :
+#         return 0
+    
+#     if dp[curr] != -1 :
+#         return dp[curr]
+    
+#     dp[curr] = arr[curr] + max(findMax(arr, curr+2), findMax(arr, curr+3))
+#     return dp[curr]
+
+
+# print(max(findMax(storage, 0), findMax(storage, 1)))
+
+
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+
+storage = list(map(int, sys.stdin.readline().split()))
+
+dp = [-1] * N
+
+dp[0] = storage[0]
+dp[1] = storage[1]
+
+maxVal = max(dp[0], dp[1])
+for i in range(2, N) :
+    maxVal = max(dp[i-1], dp[i-2] + storage[i])
+
+print(maxVal)
