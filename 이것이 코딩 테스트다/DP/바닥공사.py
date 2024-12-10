85b0ce4825df87059f28863d5a04ed1f3811ad54
yoo20370 <yoo20370@gmail.com>
2024-12-10 16:51:58 +0900

    20241210 166156

---
+++ 이것이 코딩 테스트다/DP/바닥공사.py
@@ -0,0 +1,12 @@
+import sys 
+
+N = int(sys.stdin.readline().rstrip())
+
+dp = [-1] * 1001 
+
+dp[1] = 1
+dp[2] = 3
+for i in range(3, N+1) :
+    dp[i] = (dp[i-1] + dp[i-2] * 2) % 796796
+
+print(dp[N])
