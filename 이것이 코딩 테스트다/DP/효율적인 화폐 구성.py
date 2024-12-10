c8a6c251c554b0a210090de04d621254b118571a
yoo20370 <yoo20370@gmail.com>
2024-12-10 21:22:06 +0900

    20241210 212210

--- 이것이 코딩 테스트다/DP/효율적인 화폐 구성.py
+++ 이것이 코딩 테스트다/DP/효율적인 화폐 구성.py
@@ -1,14 +1,55 @@
+# TopDown 방식 
+# import sys
+
+# N, M = map(int, sys.stdin.readline().split())
+
+# dp = [10002] * (M + 1)
+
+# coins = list()
+# for i in range(N) :
+#     coins.append(int(sys.stdin.readline().rstrip()))
+
+# def func(n) :
+#     # 계산 불가능한 경우 
+#     if n < 0 :
+#         return 10002
+#     if n == 0 :
+#         return 0
+    
+#     if dp[n] != 10002 :
+#         return dp[n]
+    
+#     for i in coins :
+#         dp[n] = min(dp[n], func(n-i)+1)    
+
+#     return dp[n]
+    
+# result = func(M)
+# if result == 10002 :
+#     print(-1)
+# else :
+#     print(result)
+
+# Bottom 방식
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
 
-
+dp = [10002] * (M+1)
 
 coins = list()
+
 for i in range(N) :
     coins.append(int(sys.stdin.readline().rstrip()))
 
-def func(N,M) :
-    dp = [10001] * 10001
+dp[0] = 0
 
-    
+for num in range(M+1) :
+    for coin in coins :
+        if num-coin >= 0 :
+            dp[num] = min(dp[num], dp[num-coin]+1)
+
+if dp[M] == 10002 :
+    print(-1)
+else :
+    print(dp[M])
