54b47fa8d3d4a1c2e447620ca1188144886e36fa
yoo20370 <yoo20370@gmail.com>
2024-12-13 20:52:55 +0900

    20241213 205259

--- 9361.py
+++ 9361.py
@@ -2,5 +2,7 @@ import sys
 
 N = int(sys.stdin.readline().rstrip())
 
-for i in range(N) :
-    print(f"Hello World, Judge {i+1}!")
+if N == 0 :
+    print("YONSEI")
+elif N == 1 :
+    print("Leading the Way to the Future")
