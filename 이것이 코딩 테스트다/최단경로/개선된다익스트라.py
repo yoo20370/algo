8d108cc20b34ada199386e438a91db2f3d2de148
yoo20370 <yoo20370@gmail.com>
2024-12-14 19:28:39 +0900

    20241214 192900

--- 이것이 코딩 테스트다/최단경로/개선된다익스트라.py
+++ 이것이 코딩 테스트다/최단경로/개선된다익스트라.py
@@ -16,7 +16,26 @@ for i in range(e) :
     node, end, val = map(int, sys.stdin.readline().split())
     graph[node].append([end,val])
 
+# 내가 작성한 코드 
+# def dijkstra(start) :
+#     minHeap = list()
+#     heapq.heappush(minHeap, (0, start))
 
+#     minDis[start] = 0
+#     while minHeap :
+#         currDisVal, startNode = heapq.heappop(minHeap) 
+#         print(currDisVal, startNode)
+#         # 방문한 노드인 경우 
+#         if visited[startNode] == True :
+#             continue
+
+#         # 방문 처리 
+#         visited[startNode] = True
+#         for endNode, moveCost in graph[startNode] :
+#             minDis[endNode] = min(minDis[endNode], currDisVal + moveCost)
+#             heapq.heappush(minHeap, (minDis[endNode], endNode) )
+
+# 이것이 코딩테스트다. 코드
 def dijkstra(start) :
     minHeap = list()
     heapq.heappush(minHeap, (0, start))
@@ -25,17 +44,16 @@ def dijkstra(start) :
     while minHeap :
         currDisVal, startNode = heapq.heappop(minHeap) 
         print(currDisVal, startNode)
-        # 방문한 노드인 경우 
-        if visited[startNode] == True :
-            continue
+        if minDis[startNode] < currDisVal :
+            continue 
 
-        # 방문 처리 
-        visited[startNode] = True
         for endNode, moveCost in graph[startNode] :
-            minDis[endNode] = min(minDis[endNode], currDisVal + moveCost)
-            heapq.heappush(minHeap, (minDis[endNode], endNode) )
+            cost = currDisVal + moveCost 
+            if cost < minDis[endNode] :
+                minDis[endNode] = cost
+                heapq.heappush(minHeap, (minDis[endNode], endNode) )
 
-dijkstra(1)
+dijkstra(start)
 
 for i in range(1,len(minDis)) :
     if minDis[i] != INF :
