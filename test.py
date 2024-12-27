b26644d3a8529c583ba1a7b1a6dc35c461876ea8
yoo20370 <yoo20370@gmail.com>
2024-12-28 00:46:49 +0900

    20241228 004642 bj 9372

--- test.py
+++ test.py
@@ -1,13 +1,40 @@
 import sys
 
-N = int(sys.stdin.readline().rstrip())
 
-dp = [-1] * (N+1)
 
-dp[0] = 0
-dp[1] = 1 
+def find_parent(parent, x) :
+    if parent[x] != x :
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
-for n in range(2, N+1) :
-    dp[n] = dp[n-2] + dp[n-1]
+def union(parent, a, b) :
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
 
-print(dp[N])
+    if a < b :
+        parent[b] = parent[a]
+    else :
+        parent[a] = parent[b]
+
+def func() -> int :
+    N, M = map(int, sys.stdin.readline().split())
+
+    parent = [i for i in range(N+1)]
+    graph = []
+
+    for _ in range(M) :
+        start, end = map(int, sys.stdin.readline().split())
+        graph.append([start, end])
+
+    cnt = 0
+    for start, end in graph :
+        if find_parent(parent, start) != find_parent(parent, end) :
+            union(parent, start, end)
+            cnt += 1
+
+    return cnt
+
+T = int(sys.stdin.readline().rstrip())
+
+for _ in range(T) :
+    print(func())
