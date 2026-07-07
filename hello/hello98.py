a5df4f60df2c2c9773e39bdae1e2ee9b92b15d8f
yoo20370 <yoo20370@gmail.com>
2026-07-07 19:59:38 +0900

    20260707

--- hello/hello98.py
+++ hello/hello98.py
@@ -5,6 +5,10 @@
 # 그 중 최소값 구하면 될 듯 
 # 깊이 우선 탐색 문제인 듯 
 
+# 깊이 우선 탐색을 수행할 경우, 최소경로가 스택 앞 부분에 문제를 틀릴 수 있음
+# 고로 깊이 우선 탐색 보다는 너비 우선 탐색을 통해서 처리하는게 맞는 듯
+# 그 이유는 큐 같은 경우는 FIFO 이므로 앞에서 부터 차근차근 처리하므로 괜찮음 
+
 from collections import deque
 
 INF = int(1e9)
@@ -34,8 +38,7 @@ def solution(begin, target, words):
             continue
         
         visited.add(currentWord)
-        # 도달 가능한 여러 가지 수 중 하나일 뿐이므로 함수 자체를 종료시키면 안 됨 
-        # 해당 단어는 끝났으며 현재 경우에 대해서 추가적으로 스택에 넣을 필요 없으므로 continue
+    
         if currentWord == target :
             minChangeCount = min(minChangeCount, currentCount)
             continue
